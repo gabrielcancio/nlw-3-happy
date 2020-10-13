20e6f61cffc57cb4cba8af60fcf2f9b02a567794
gabrielcancio <gabriel.cancio02@gmail.com>
2020-10-13 07:33:47 -0300

    Starting the orphanage map page

--- web/src/routes.tsx
+++ web/src/routes.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Landing from './Pages/Landing';
+import OrphanagesMap from './Pages/OrphanagesMap';
 
 const Routes: React.FC = () => (
     <BrowserRouter>
         <Switch>
             <Route path="/" exact component={Landing} />
+            <Route path="/app" component={OrphanagesMap} />
         </Switch>   
     </BrowserRouter>
 )

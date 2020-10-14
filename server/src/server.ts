977db2a56c2045a975ef653b1b54c6ecb18c4a1d
gabrielcancio <gabriel.cancio02@gmail.com>
2020-10-14 19:31:16 -0300

    Making static files route

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import path from 'path';
 import './database/connection'; // Importando a conexão com o banco de dados
 
 import routes from './routes';
@@ -8,5 +9,7 @@ const app = express();
 
 app.use(express.json()); // Habilita o entendimento do json pela aplicação
 app.use(routes);
+
+app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads'))); // Rota para servir imagens utlizando o método estatic do express qeu recebe o caminho para a pasta de uploads como parametro
 
 app.listen(process.env.PORT || 3333);

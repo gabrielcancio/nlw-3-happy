b63bc155324b8d23b995ef7097a7a0844a2f7859
gabrielcancio <gabriel.cancio02@gmail.com>
2020-10-17 18:30:46 -0300

    Connecting with API

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import Routes from './routes';
 
-import './styles/global.css'
+import './styles/global.css';
+import 'leaflet/dist/leaflet.css'; // Como esta importação está senod usada em mais de tres lugares deve ficar no App.tsx
 
 function App() {
   return (

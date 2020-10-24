b21ae80ea4a3019f4b34f66e6d7f865509debe74
gabrielcancio <gabriel.cancio02@gmail.com>
2020-10-24 20:04:20 -0300

    Trying to get better the baseURL

--- Readme.md
+++ Readme.md
@@ -104,5 +104,14 @@ Or
 ```bash
 $ npm start
 ``` 
+
+*PS: You neend change the baseURL to IP of your computer on path mobile/src/services/api.ts insed of api constant, on baseURL key. Like this:*
+
+```javascript
+#Replace "yourIP" to IP of your PC
+const api = axios.create({
+    baseURL: `http://yourIP:3333`
+});
+```
 ---
 Developed with :yellow_heart: by gabriel Cancio!
--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,10 +1,8 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { useFonts } from 'expo-font';
 import { Nunito_600SemiBold, Nunito_700Bold, Nunito_800ExtraBold } from '@expo-google-fonts/nunito';
 
 import Routes from './src/routes';
-
 
 export default function App() {
   const [fontsLoaded] = useFonts({ // Recebe um objeto com as fontes e retorna uma variável do tipo boolean que informa se as fontes ja foram carregadas ou não
